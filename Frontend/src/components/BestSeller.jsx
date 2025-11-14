178b052d357c14d5c19cd25a351693d6be34b40a
Soumyadip Barik <soumyadipbarik735@gmail.com>
2025-11-14 23:45:26 +0530

    first commit

--- Frontend/src/components/BestSeller.jsx
+++ Frontend/src/components/BestSeller.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import Title from "./Title";
+import ProductItem from "./ProductItem";
 
 const BestSeller = () => {
   const { products } = useContext(ShopContext);
@@ -23,7 +24,17 @@ const BestSeller = () => {
         </p>
       </div>
 
-      
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {bestSeller.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            name={item.name}
+            image={item.image}
+            price={item.price}
+          />
+        ))}
+      </div>
     </div>
   );
 };
